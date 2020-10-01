0cd6f2f51e5702e371418aeab3d7d9080f9c39e9
Tim Schutte <ts549@njit.edu>
2020-10-01 02:09:08 +0000

    Fix minor issue with buttons

--- static/hello.js
+++ static/hello.js
@@ -4,7 +4,7 @@ alert("Hello, world!");
 
 window.onload = function(){
     
-    document.getElementById("submit_button").onclick = function(){
+    document.getElementById("awesome_button").onclick = function(){
         alert("I clicked the button!");
     }
     document.getElementById("submit_button").onclick = function(){
